30f4f9d29f64d816df29c7faaa0d46bab61b50d0
Reddy0309 <rtejaswini2003@gmail..com>
2025-02-09 16:06:14 +0530

    Fix missing dependency vite-plugin-env-compatible

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import envCompatible from 'vite-plugin-env-compatible'
+import vitePluginEnvCompatible from 'vite-plugin-env-compatible';
 
 // https://vite.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
   plugins: [react(),
     envCompatible(),
+    vitePluginEnvCompatible(),
   ],
 })
