2c5dd5c613a91031fe88c025e7dc98de028ca32c
Reddy0309 <rtejaswini2003@gmail..com>
2025-02-09 16:12:25 +0530

    Fixed jspdf dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import envCompatible from 'vite-plugin-env-compatible'
 import vitePluginEnvCompatible from 'vite-plugin-env-compatible';
 
 // https://vite.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
+  build: {
+    rollupOptions: {
+      external: ['jspdf'], // Add jspdf to external to prevent build errors
+    },
+  },
   plugins: [react(),
-    envCompatible(),
     vitePluginEnvCompatible(),
   ],
 })
